550b65ab21b740853b3069178d6b0542034dbedd
Brandon Feltz <brfeltz@e1z2r3p31.42.us.org>
2019-03-05 17:41:05 -0800

    testing strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,12 +6,12 @@
 /*   By: brfeltz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 12:23:28 by brfeltz           #+#    #+#             */
-/*   Updated: 2019/03/04 14:02:01 by brfeltz          ###   ########.fr       */
+/*   Updated: 2019/03/05 17:39:58 by brfeltz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+/*
 static char		*ft_place_words(char const *s, char c, int *index)
 {
 	int		i;
@@ -56,6 +56,59 @@ char			**ft_strsplit(char const *s, char c)
 	if (sentence)
 		while (i < count)
 			sentence[i++] = ft_place_words((char*)s, c, &k);
+	sentence[i] = 0;
+	return (sentence);
+}
+*/
+
+static char		*ft_place_words(char const *s, char c, int *index)
+{
+	int		i;
+	char	*word;
+	int		start;
+
+	i = 0;
+	while (s[*index] == c && s[*index])
+		(*index)++;
+	start = *index;
+	while (s[*index] != c && s[*index])
+		(*index)++;
+	if (!(word = ft_strnew(*index - start)))
+		return (NULL);
+	while (start < *index)
+	{
+		word[i] = s[start];
+		i++;
+		start++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char			**ft_strsplit(char const *s, char c)
+{
+	char	**sentence;
+	int		i;
+	int		count;
+	int		k;
+
+	k = 0;
+	count = 0;
+	i = 0;
+	if (!s || !c)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+			count++;
+		i++;
+	}
+	i = 0;
+	sentence = (char **)malloc((count + 1) * sizeof(char *));
+	if (!sentence)
+		return (NULL);
+	while (i < count)
+		sentence[i++] = ft_place_words(s, c, &k);
 	sentence[i] = 0;
 	return (sentence);
 }
