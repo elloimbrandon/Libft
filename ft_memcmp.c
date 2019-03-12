eb5606de8530379ac486ceb3575a31fcbed997ea
Brandon Feltz <brfeltz@e1z3r4p3.42.us.org>
2019-03-11 18:07:00 -0700

    memcmp fixed

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: brfeltz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 14:44:32 by brfeltz           #+#    #+#             */
-/*   Updated: 2019/02/20 12:50:00 by brfeltz          ###   ########.fr       */
+/*   Updated: 2019/03/11 18:05:00 by brfeltz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,17 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const unsigned char *str1;
-	const unsigned char *str2;
+	unsigned char *str1;
+	unsigned char *str2;
 
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	if (s1 == s2 || n == 0)
 		return (0);
-	str1 = (const unsigned char *)s1;
-	str2 = (const unsigned char *)s2;
-	while (n--)
+	while (*str1 == *str2 && --n)
 	{
-		if (*str1 != *str2)
-			return (*str1 - *str2);
-		if (n)
-		{
-			str1++;
-			str2++;
-		}
+		str1++;
+		str2++;
 	}
-	return (0);
+	return (*str1 - *str2);
 }
