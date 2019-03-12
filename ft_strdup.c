2bb25440853be202ff644d08ca77167a234ad0fc
Brandon Feltz <brfeltz@e1z3r4p3.42.us.org>
2019-03-11 20:08:13 -0700

    2strdup test

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: brfeltz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/31 22:58:22 by brfeltz           #+#    #+#             */
-/*   Updated: 2019/03/11 20:04:58 by brfeltz          ###   ########.fr       */
+/*   Updated: 2019/03/11 20:07:34 by brfeltz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ char	*ft_strdup(const char *src)
 {
 	char	*s2;
 
-	s2 = ft_strnew(ft_strlen(s));
+	s2 = ft_strnew(ft_strlen(src));
 	if (s2)
-		return (ft_strcpy(s2, (char*)s));
+		return (ft_strcpy(s2, (char*)src));
 	return (0);
 }
