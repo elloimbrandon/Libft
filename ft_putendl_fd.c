7971854e6f89935bb8cda549a0f2dd9dfd837b63
Brandon Feltz <brfeltz@e1z3r4p3.42.us.org>
2019-03-11 18:24:51 -0700

    fixed cast ft_putendl_fd

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: brfeltz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/19 22:06:32 by brfeltz           #+#    #+#             */
-/*   Updated: 2019/02/20 18:01:31 by brfeltz          ###   ########.fr       */
+/*   Updated: 2019/03/11 18:23:01 by brfeltz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,6 @@
 
 void	ft_putendl_fd(char const *s, int fd)
 {
-	ft_putstr_fd((char*)s, fd);
+	ft_putstr_fd(s, fd);
 	write(fd, "\n", 1);
 }
