8497bc3d829e35a8ea84cc58a3982144109e0f28
Brandon Feltz <brfeltz@e1z2r3p8.42.us.org>
2019-05-02 19:51:48 -0700

    .

--- ft_putstr.c
+++ ft_putstr.c
@@ -6,20 +6,13 @@
 /*   By: brfeltz <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 15:42:30 by brfeltz           #+#    #+#             */
-/*   Updated: 2019/03/05 15:57:56 by brfeltz          ###   ########.fr       */
+/*   Updated: 2019/05/02 19:51:14 by brfeltz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putstr(char const *str)
+void	ft_putstr(const char *str)
 {
-	int i;
-
-	i = 0;
-	while (str[i])
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
+	write(1, str, ft_strlen((char *)str));
 }
